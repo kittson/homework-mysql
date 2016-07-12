0df37470e1b43f40395043deeefa637afcc0de05
kittson <gazorna@gmail.com>
2016-07-12 14:54:59 -0500

    cleaned up files, newest homework

---
+++ bamcustomer.js
@@ -0,0 +1,89 @@
+var inquirer = require('inquirer');
+var syncoprompt = require('syncoprompt');
+var mysql = require('mysql');
+var connection;
+var itemNum;
+
+var startBamazon = function() {
+	inquirer.prompt({
+		name: "pickItem",
+		type: "input",
+		message: "What item would you like to purchase?  Choose by Item #\n",
+	}).then(function(answer) {
+		itemNum = Number(answer.pickItem);				
+		var queryString = 'SELECT ItemID, ProductName, PriceCost, StockQuantity FROM products WHERE ItemID = ' + itemNum;
+		connection.query(queryString, function(err, rows, fields) {
+			if (err) { 
+				throw err;
+			}
+			else {				
+				console.log('you chose #' + rows[0].ItemID + ' ' + rows[0].ProductName + ' at $' + rows[0].PriceCost); 
+				inquirer.prompt({ //to find out how many they want
+					name: "howMany",
+					type: "input",
+					message: "How many of " + rows[0].ProductName + " do you wish?\n" 
+					+ "We have " + rows[0].StockQuantity + " available\n",
+				}).then(function(answer) {
+					var numAvail = Number(rows[0].StockQuantity);
+					var cost = Number(rows[0].PriceCost);
+					if(answer.howMany <= numAvail) {
+						console.log("That's $" + (answer.howMany * cost));
+						var left = numAvail - answer.howMany;
+						numLeft = Number(left);						
+						var queryString = 'UPDATE products SET StockQuantity=' + numLeft + ' WHERE ItemID = ' + itemNum;						
+						connection.query(queryString, function(err, rows, fields) {
+							if (err) throw err;
+							console.log("Updated ");
+							connection.end();
+						});
+					}//if
+				});//end of howMany
+			}//else
+		});//connection		
+	});//then from inquirer
+};//startBamazon
+
+var showProducts = function(){	
+	var queryString = 'SELECT ItemID, ProductName, DepartmentName, PriceCost FROM products';
+	connection.query(queryString, function(err, rows, fields) {
+		if (err) throw err;
+		for (var i in rows) {
+			console.log('Item #' + rows[i].ItemID + ', ' 
+				+ rows[i].ProductName 
+				+ ', Department: '  
+				+ rows[i].DepartmentName 
+				+ ', $' + rows[i].PriceCost);
+		}
+	});
+	startBamazon();		
+};
+
+var getCreds = function(){
+	var userName;
+	var passWord;
+	inquirer.prompt({
+		name: "username",
+		type: "input",
+		message: "Username for SQL database access:\n",
+	}).then(function(answer) {
+		userName = answer.username;
+		inquirer.prompt({
+			name: "pass",
+			type: "password",
+			message: "Password:\n",
+		}).then(function(answer) {
+			passWord = answer.pass;
+			connection = mysql.createConnection({
+				host     : 'localhost',
+				user     : userName,
+				password : passWord,
+				database : 'bamazon'
+			});
+			connection.connect();
+		//start the program
+		showProducts();
+		});//get password
+	});// get username		
+};//function getCreds()
+
+getCreds();
